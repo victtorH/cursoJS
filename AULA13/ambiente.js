365ae2b9311661f43c026cb947c9d8373dd188bb
Victtor Hugo Estevam <estevam.victtor@gmail.com>
2024-05-22 20:08:54 -0300

    AULA13

---
+++ AULA13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 6 )
+
+/*while(c <= 6){
+    console.log(`passo ${c}`)
+    c += 1
+}*/
+
+/*console.log('passo1')
+console.log('passo1')
+console.log('passo1')
+console.log('passo1')
+console.log('passo1')
+console.log('passo1')*/
+
---
+++ AULA13/ambientedois.js
@@ -0,0 +1,5 @@
+console.log('inicio do código')
+ for(var c =1;c<=6;c++){
+    console.log(`${c}`)
+ }
+ console.log('FIM')
