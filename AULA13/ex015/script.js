2b497377afacc9c15a95aa6709794ab78aa29355
Victtor Hugo Estevam <estevam.victtor@gmail.com>
2024-05-22 13:17:16 -0300

    termino_ex15

--- AULA13/ex015/script.js
+++ AULA13/ex015/script.js
@@ -1,8 +1,6 @@
 var carr = window.document.getElementById("carregar")
 var nas = window.document.getElementById("nasceu")
-var sex = window.document.getElementById("sexo")
-var masc = window.document.getElementById("masculino")
-var fem = window.document.getElementById("feminino")
+var sexo = window.document.getElementsByName("sex")
 var ver = window.document.getElementById("verificar")
 
 ver.addEventListener("click",clique)
@@ -13,5 +11,26 @@ var ano = Data.getFullYear()
 function clique(){
    var nasc = Number(nas.value)
    var idade = ano - nasc
-   carr.innerHTML = `Voce tem ${idade}`
+   var genero = " "
+   var pessoa = " "
+   if (sexo[0].checked){
+       genero = "masculino"
+      
+   }else{
+       genero = "feminino"
+      
+   }
+   if (idade < 13){
+         pessoa = "criança" 
+      }else if( idade < 18){
+         pessoa = "adolecente"
+      }else if( idade < 25){
+         pessoa = "jovem adulto"
+      }else if( idade < 46){
+         pessoa = "aduto"
+      }else{
+         pessoa = "idoso"
+      }
+   carr.innerHTML = `Voce é do sexo ${genero} tem ${idade} anos e é ${pessoa}`
+   
 }
