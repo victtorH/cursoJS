940216fcfa95ce152ef7e4271de9bb26edd6c79b
Victtor Hugo Estevam <estevam.victtor@gmail.com>
2024-06-13 01:10:50 -0300

    termino_do_curso

--- AULA15/ambiente.js
+++ AULA15/ambiente.js
@@ -5,12 +5,12 @@ console.log(`minhas variaveis compostas são ${numeros}`)*/
 /*
 console.log(`${numeros.length}`)*/
 
-numeros.sort()
-console.log(numeros)
+// numeros.sort()
+// console.log(numeros)
 /*
 for(contador in numeros){
     console.log(`o espaço ${contador} recebe a variavel ${numeros[contador]}`)
 }*/
 
-/*console.log(numeros.indexOf(2))
-console.log(numeros.indexOf(13))*/
+console.log(numeros.indexOf(2))
+console.log(numeros.indexOf(13))
--- AULA16ex/ex18/scripts.js
+++ AULA16ex/ex18/scripts.js
@@ -4,48 +4,52 @@ var valo = document.getElementById('valores')
 var ver = document.getElementById('verificar')
 var res = document.getElementById('resposta')
 
-acre.addEventListener('click',clique)
+acre.addEventListener('click', clique)
 ver.addEventListener('click', click)
+var array = []
 
-let array = []
 
-function clique(){
- 
-    let rece = Number(rec.value)
-    
-    if(rece <= 0 || rece > 100){
-        window.alert('[ERRO] valor invalido')
-    }else if( array.indexOf(rece) != -1 ){
-        window.alert(`valor ${rece} já encontrado`)
-    }else{
-        array.push(rece)
-        let repositor = document.createElement('option')
-        repositor.text =  `o numero ${rece} foi adicionado`
-        valo.appendChild(repositor)
-        
-}
- console.log(array)
- 
-}
 
-function click(){
-    array.sort()
-
-    res.innerHTML = `<h1> AO TODO, FORAM ${array.length} NUMEROS ADICIONADOS  </h1>`
-
-    
-
-    res.innerHTML += `<h3> o menor valor adicionado foi ${array[0]}</h3>`
-    
-    for( contador in array.length){
-        if( contador = array){
-            res.innerHTML = `o maior valor é ${array[contador]}`
-        }
+function clique() {
+    var cap = Number(rec.value)
+    if (cap <= 0 || cap > 100) {
+        window.alert('[ERRO]numero digitado invalido.')
+    } else if (array.indexOf(cap) != -1) {
+        window.alert('Esse item já foi inserido.')
+    } else {
+        array.push(cap)
+        var posi = document.createElement('option')
+        posi.text = `valor ${cap} adicionado`
+        valo.appendChild(posi)
     }
-        rec.value = ''
-        rec.focus()
+    rec.value = ''
+    rec.focus()
+    res.innerHTML = ' '
 
-    console.log(array)
-    }
+}
+
+function click() {
+    let quan = array.length
+
+    let maior = array[0]
+    let menor = array[0]
+    let soma = array[0]
     
+    for (let conta in array) {
+        if (array[conta] > maior)
+            maior = array[conta]
+        if (array[conta] < menor)
+            menor = array[conta]
+        if (array[conta] < soma)
+            soma = array[conta] + soma
 
+    }
+let média = soma / quan
+
+    res.innerHTML += `<h3>Foram acresentados ${quan} numeros</h3>`
+    res.innerHTML += `<h3>${maior} é o maior numeros</h3>`
+    res.innerHTML += `<h3> ${menor} é o menor numeros</h3>`
+    res.innerHTML += `<h3> ${soma} é a soma dos numeros</h3>`
+     res.innerHTML += `<h3> ${média} é a média dos numeros</h3>`
+}
+console.log(array)
